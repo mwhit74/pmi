afa0bde6a84ba5b3d2b12483a63db5fa03c15dfb
mwhit74 <mwhit74@gmail.com>
2016-08-31 15:56:55 -0500

    Testing strain field iteration options

---
+++ test1.py
@@ -0,0 +1,25 @@
+import math
+import matplotlib.pyplot as plt
+
+c_start = 0.0
+c_end = 20.0
+num_steps = 100
+c_step = (c_end-c_start)/num_steps
+
+c = []
+phi = []
+
+for x in range(num_steps):
+    if x == 0:
+        c.append(c_step)
+    else:
+        c.append(c[x-1] + c_step)
+    phi.append(0.003/c[x])
+    
+plt.plot(phi,c)
+
+plt.show()
+    
+
+    
+    
